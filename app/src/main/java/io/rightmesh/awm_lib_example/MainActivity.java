78336fbfed06f340932d7d5463a11df709e90d33
Jason Ernst <jason@left.io>
2018-09-28 17:34:58 -0700

    changed from using context to activity

--- app/src/main/java/io/rightmesh/awm_lib_example/MainActivity.java
+++ app/src/main/java/io/rightmesh/awm_lib_example/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        awsc = new AndroidWirelessStatsCollector(getApplicationContext());
+        awsc = new AndroidWirelessStatsCollector(this);
         awsc.start();
 
         txtBtDevices = findViewById(R.id.btDevices);
