4f2e5aee389dee4629161c28789fda7881d0ee88
Jason Ernst <jason@left.io>
2018-12-07 13:44:37 -0800

    Added Wi-Fi AP detection to the UI

--- app/src/main/java/io/rightmesh/awm_lib_example/MainActivity.java
+++ app/src/main/java/io/rightmesh/awm_lib_example/MainActivity.java
@@ -11,12 +11,14 @@ import com.anadeainc.rxbus.Subscribe;
 import io.rightmesh.awm.AndroidWirelessStatsCollector;
 import io.rightmesh.awm.BluetoothStats;
 import io.rightmesh.awm.GPSStats;
+import io.rightmesh.awm.WiFiStats;
 
 public class MainActivity extends AppCompatActivity {
 
     private AndroidWirelessStatsCollector awsc;
     protected Bus eventBus = BusProvider.getInstance();
     private TextView txtBtDevices;
+    private TextView txtWifiDevices;
     private TextView txtGPS;
 
     @Override
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         awsc.start();
 
         txtBtDevices = findViewById(R.id.btDevices);
+        txtWifiDevices = findViewById(R.id.wifiDevices);
         txtGPS = findViewById(R.id.gpsCoords);
         eventBus.register(this);
     }
@@ -35,11 +38,21 @@ public class MainActivity extends AppCompatActivity {
     @Subscribe
     public void updateBTDevices(BluetoothStats btStats) {
         String status = "btDevices: ";
-        status = status + btStats.macs.size();
-        for(String mac : btStats.macs) {
+        status = status + btStats.getMacs().size();
+        for(String mac : btStats.getMacs()) {
             status = status + "\n" + mac;
         }
         txtBtDevices.setText(status);
+    }
+
+    @Subscribe
+    public void updateWiFiDevices(WiFiStats wifiStats) {
+        String status = "wifiDevices: ";
+        status = status + wifiStats.getMacs().size();
+        for(String mac : wifiStats.getMacs()) {
+            status = status + "\n" + mac;
+        }
+        txtWifiDevices.setText(status);
     }
 
     @Subscribe
